f3591fcd5fb6e71b29bdb31764fb51fb75edcd07
imutsav11 <138605376+imutsav11@users.noreply.github.com>
2024-04-16 22:28:37 +0530

    email setting removed

--- hrmanagement/hrmanagement/settings.py
+++ hrmanagement/hrmanagement/settings.py
@@ -147,12 +147,7 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 #email settings
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'utaavthakkar@gmail.com'
-EMAIL_HOST_PASSWORD = 'qmrcpbdlpmggejry'
+
 
 LOGIN_REDIRECT_URL = '/path_to_the_page'
 LOGIN_URL = '/user/login'
